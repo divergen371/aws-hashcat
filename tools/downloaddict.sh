ed5b6389b9f37d0691c4712aa3f2259732eebed1
divergen371 <divergen@protonmail.com>
2020-10-17 22:30:14 +0900

    URL typo fix

--- tools/downloaddict.sh
+++ tools/downloaddict.sh
@@ -2,7 +2,7 @@
 
 mkdir wordlist
 git clone https://github.com/danielmiessler/SecLists.git ~/wordlist/seclists
-wget -nH https://download.skullsecurity.org/passwords/rockyou.txt.bz2 -O ~/wordlist/rockyou.txt.bz2
+wget -nH http://downloads.skullsecurity.org/passwords/rockyou.txt.bz2 -O ~/wordlist/rockyou.txt.bz2
 
 cd wordlist
 bunzip2 ./rockyou.txt.bz2
